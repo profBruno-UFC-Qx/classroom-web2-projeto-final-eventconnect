3f51b6c5e410dd6ad2a5c23c7b076ebab88e4baa
Jeferson Aires <jefersonaires.jittechnology@gmail.com>
2026-01-18 18:37:31 -0300

    removendo o back com strapi

--- backend/projeto-final-web/src/api/inscricao/controllers/inscricao.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * inscricao controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::inscricao.inscricao');
